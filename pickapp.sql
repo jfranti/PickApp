edc9953eb6c99d1222e00bb14ee6971c1e4532fe
John <student@epicodus.com>
2015-03-31 11:42:32 -0700

    Update database.

--- pickapp.sql
+++ pickapp.sql
@@ -150,6 +150,40 @@ ALTER SEQUENCE events_id_seq OWNED BY events.id;
 
 
 --
+-- Name: events_players; Type: TABLE; Schema: public; Owner: Guest; Tablespace: 
+--
+
+CREATE TABLE events_players (
+    id integer NOT NULL,
+    event_id integer,
+    player_id integer
+);
+
+
+ALTER TABLE events_players OWNER TO "Guest";
+
+--
+-- Name: events_players_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE events_players_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE events_players_id_seq OWNER TO "Guest";
+
+--
+-- Name: events_players_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE events_players_id_seq OWNED BY events_players.id;
+
+
+--
 -- Name: events_users; Type: TABLE; Schema: public; Owner: Guest; Tablespace: 
 --
 
@@ -181,6 +215,39 @@ ALTER TABLE events_users_id_seq OWNER TO "Guest";
 --
 
 ALTER SEQUENCE events_users_id_seq OWNED BY events_users.id;
+
+
+--
+-- Name: players; Type: TABLE; Schema: public; Owner: Guest; Tablespace: 
+--
+
+CREATE TABLE players (
+    id integer NOT NULL,
+    name character varying
+);
+
+
+ALTER TABLE players OWNER TO "Guest";
+
+--
+-- Name: players_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE players_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE players_id_seq OWNER TO "Guest";
+
+--
+-- Name: players_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE players_id_seq OWNED BY players.id;
 
 
 --
@@ -242,7 +309,21 @@ ALTER TABLE ONLY events ALTER COLUMN id SET DEFAULT nextval('events_id_seq'::reg
 -- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
 --
 
+ALTER TABLE ONLY events_players ALTER COLUMN id SET DEFAULT nextval('events_players_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
 ALTER TABLE ONLY events_users ALTER COLUMN id SET DEFAULT nextval('events_users_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY players ALTER COLUMN id SET DEFAULT nextval('players_id_seq'::regclass);
 
 
 --
@@ -298,6 +379,21 @@ SELECT pg_catalog.setval('events_id_seq', 1, false);
 
 
 --
+-- Data for Name: events_players; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY events_players (id, event_id, player_id) FROM stdin;
+\.
+
+
+--
+-- Name: events_players_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('events_players_id_seq', 1, false);
+
+
+--
 -- Data for Name: events_users; Type: TABLE DATA; Schema: public; Owner: Guest
 --
 
@@ -310,6 +406,21 @@ COPY events_users (id, user_id, event_id) FROM stdin;
 --
 
 SELECT pg_catalog.setval('events_users_id_seq', 1, false);
+
+
+--
+-- Data for Name: players; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY players (id, name) FROM stdin;
+\.
+
+
+--
+-- Name: players_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('players_id_seq', 1, false);
 
 
 --
@@ -352,11 +463,27 @@ ALTER TABLE ONLY events
 
 
 --
+-- Name: events_players_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace: 
+--
+
+ALTER TABLE ONLY events_players
+    ADD CONSTRAINT events_players_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: events_users_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace: 
 --
 
 ALTER TABLE ONLY events_users
     ADD CONSTRAINT events_users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: players_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace: 
+--
+
+ALTER TABLE ONLY players
+    ADD CONSTRAINT players_pkey PRIMARY KEY (id);
 
 
 --
